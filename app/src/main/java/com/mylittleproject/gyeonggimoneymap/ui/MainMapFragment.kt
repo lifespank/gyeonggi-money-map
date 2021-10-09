e59115da37304f7776411121bfaf1345afc21a28
K007_김수명 <admin@sumyeong.kim>
2021-10-09 23:07:08 +0900

    [FEAT] 초기 지도 설정

--- app/src/main/java/com/mylittleproject/gyeonggimoneymap/ui/MainMapFragment.kt
+++ app/src/main/java/com/mylittleproject/gyeonggimoneymap/ui/MainMapFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.mylittleproject.gyeonggimoneymap.R
 import com.mylittleproject.gyeonggimoneymap.databinding.FragmentMapBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
@@ -14,7 +16,7 @@ import com.naver.maps.map.OnMapReadyCallback
 class MainMapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
-    private lateinit var naverMap : NaverMap
+    private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,5 +44,12 @@ class MainMapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
+        configMap()
+    }
+
+    private fun configMap() {
+        naverMap.minZoom = 15.0
+        naverMap.maxZoom = 17.0
+        naverMap.extent = LatLngBounds(LatLng(31.43, 122.37), LatLng(44.35, 132.0))
     }
 }
