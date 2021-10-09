9bdb6d1ad664454e43c7735b60189f1d3d5768c1
K007_김수명 <admin@sumyeong.kim>
2021-10-09 15:59:16 +0900

    [FEAT] 지도 프래그먼트에 카카오맵 표시

--- app/src/main/java/com/mylittleproject/gyeonggimoneymap/ui/MapFragment.kt
+++ app/src/main/java/com/mylittleproject/gyeonggimoneymap/ui/MapFragment.kt
@@ -1,4 +1,4 @@
-package com.mylittleproject.gyeonggimoneymap
+package com.mylittleproject.gyeonggimoneymap.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mylittleproject.gyeonggimoneymap.databinding.FragmentMapBinding
+import net.daum.mf.map.api.MapView
 
 class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
@@ -18,6 +19,13 @@ class MapFragment : Fragment() {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapView = MapView(requireContext())
+        val mapViewContainer = binding.root as ViewGroup
+        mapViewContainer.addView(mapView)
     }
 
     override fun onDestroyView() {
