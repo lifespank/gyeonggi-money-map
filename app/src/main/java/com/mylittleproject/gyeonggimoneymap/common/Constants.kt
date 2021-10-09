0e125a1cc83f8937e6c3066cf6b77b2fa2e7cd31
K007_김수명 <admin@sumyeong.kim>
2021-10-09 23:33:10 +0900

    [FEAT] 현재 위치로 카메라 이동 버튼 추가

--- app/src/main/java/com/mylittleproject/gyeonggimoneymap/common/Constants.kt
+++ app/src/main/java/com/mylittleproject/gyeonggimoneymap/common/Constants.kt
@@ -1,1 +1,3 @@
 package com.mylittleproject.gyeonggimoneymap.common
+
+const val LOCATION_PERMISSION_REQUEST_CODE = 1000
--- app/src/main/java/com/mylittleproject/gyeonggimoneymap/ui/MainMapFragment.kt
+++ app/src/main/java/com/mylittleproject/gyeonggimoneymap/ui/MainMapFragment.kt
@@ -6,17 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mylittleproject.gyeonggimoneymap.R
+import com.mylittleproject.gyeonggimoneymap.common.LOCATION_PERMISSION_REQUEST_CODE
 import com.mylittleproject.gyeonggimoneymap.databinding.FragmentMapBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
 class MainMapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
     private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        locationSource =
+            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,14 +52,30 @@ class MainMapFragment : Fragment(), OnMapReadyCallback {
         _binding = null
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
+        naverMap.locationSource = locationSource
         configMap()
     }
 
     private fun configMap() {
         naverMap.minZoom = 15.0
         naverMap.maxZoom = 17.0
+        naverMap.uiSettings.isLocationButtonEnabled = true
         naverMap.extent = LatLngBounds(LatLng(31.43, 122.37), LatLng(44.35, 132.0))
     }
 }
