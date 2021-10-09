2a17c28ec3e730a96e09b390fffd347a455f9ea2
K007_김수명 <admin@sumyeong.kim>
2021-10-09 14:30:01 +0900

    [INIT] 시작 커밋

---
+++ app/src/main/java/com/mylittleproject/gyeonggimoneymap/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mylittleproject.gyeonggimoneymap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
