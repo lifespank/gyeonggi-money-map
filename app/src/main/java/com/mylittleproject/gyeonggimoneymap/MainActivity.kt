d4795c4897dec8dc84c8c3cfc9ba3b8882b2a8bd
K007_김수명 <admin@sumyeong.kim>
2021-10-10 23:17:27 +0900

    [FIX] Status bar 반투명, Progress Indicator 색 변경

--- app/src/main/java/com/mylittleproject/gyeonggimoneymap/MainActivity.kt
+++ app/src/main/java/com/mylittleproject/gyeonggimoneymap/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mylittleproject.gyeonggimoneymap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
